1dc4e537224676da22d481d7729cd80467b5af4e
Karla Sierra <ksierra@ucol.mx>
2020-03-03 18:56:08 -0600

    Clase Hotel avances

---
+++ Hotel.js
@@ -0,0 +1,21 @@
+import Reservacion from "./Reservacion.js"
+class Hotel{
+    /**
+     * 
+     * @param {string} nombre 
+     */
+    constructor(nombre){
+        this.nombre = nombre;
+        this.reservaciones = new Array;
+    }
+    getNumHuespedes(){
+        var HuespedesTotal=0;
+        this.reservaciones.forEach(reservacion=>{
+            HuespedesTotal = reservacion.huespedes.length + HuespedesTotal;  
+        });
+        return HuespedesTotal;
+    }
+    print(){
+        return(this.print());
+    }
+}
