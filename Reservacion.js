e57846d550027e94d845321f6284b7a9f70cc77d
Karla Sierra <ksierra@ucol.mx>
2020-03-03 18:20:16 -0600

    Clase Reservacion avance

---
+++ Reservacion.js
@@ -0,0 +1,27 @@
+import Huesped from "./Huesped.js"
+export default class Reservacion{
+    /**
+     * 
+     * @param {number} numeroHabitacion 
+     * @param {Date} fechaLlegada 
+     * @param {number} noches 
+     */
+    constructor(numeroHabitacion, fechaLlegada, noches){
+        this.numeroHabitacion = numeroHabitacion;
+        this.fechaLlegada = fechaLlegada;
+        this.noches = noches;
+        this.huespedes = new Array();
+    }
+    getFechaFormatoCorto(){
+        return `${this.fechaLlegada.getDate()}/${this.fechaLlegada.getMonth()}/${this.fechaLlegada.getFullYear()}`;
+    }
+    addHuesped(huesped){
+        this.huespedes.push(huesped);
+    }
+    getNumHuespedes(){
+        return (this.huespedes.length);
+    }
+    print(){
+        return (`Habitacion ${this.numeroHabitacion}\n Fecha de llegada ${this.fechaLlegada.getFechaFormatoCorto()}\n Noches reservadas ${this.noches}`);
+    }
+}
